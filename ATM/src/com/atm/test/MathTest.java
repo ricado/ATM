f77d073546ea2003e2cface69b25615cb33b4434
ricado <1169833934@qq.com>
2015-08-24 11:23:46 +0800

    Revert "Revert "修改""
    This reverts commit 892ab51c4285f558f65120a3efef53ae93e4a179.

--- ATM/src/com/atm/test/MathTest.java
+++
@@ -1,8 +0,0 @@
-package com.atm.test;
-
-public class MathTest {
-	public static void main(String[] args) {
-		int number = (int)(Math.random()*10);
-		System.out.println(number);
-	}
-}
--- ATM/src/com/atm/util/LogUtil.java
+++
@@ -1,9 +0,0 @@
-package com.atm.util;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-public interface LogUtil {
-	public static final Logger log = 
-			LoggerFactory.getLogger(LogUtil.class);
-}
